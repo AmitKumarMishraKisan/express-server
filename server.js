eeb7c48562a66a11ffcd74d27716fa56fc5e2ac5
amitmishra795 <amitmishra795@gmail.com>
2024-08-25 22:13:40 +0000

    Updated the server file

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ let app = express()
 
 app.use(express.static(__dirname + '/build'))
 
+app.get("/", (req, res) => {
+    res.send("Hello World!!");
+})
+
 app.listen(3000, () => {
     console.log('Listening at port 3000');
 })
